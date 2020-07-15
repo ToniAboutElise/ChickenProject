be6830e3ce5fabfc64dad936d28d6fd1722f440a
darktoni94 <tonidominguezreig@gmail.com>
2020-07-15 22:34:18 +0200

    Creates new scean and first scripts

---
+++ Chicken Project/Assets/Scripts/Chicken.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chicken : MonoBehaviour
+{
+    public enum Color
+    {
+        Yellow,
+        Red,
+        Blue,
+        Green,
+        White,
+        Grey
+    }
+
+    public enum Head
+    {
+        None,
+        ArmyHelmet,
+        BaseballCap
+    }
+
+    public enum Eyes
+    {
+        None,
+        GlassesWhite,
+        GlassesBlue,
+        GlassesOrange,
+        GlassesPink,
+        SunGlassesWhite,
+        SunGlassesBlue,
+        SunGlassesOrange,
+        SunGlassesPink
+    }
+
+    public enum Beak
+    {
+        Yellow,
+        Red,
+        Blue,
+        Green,
+        White,
+        Grey
+    }
+}

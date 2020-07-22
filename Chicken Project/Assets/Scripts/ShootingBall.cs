bb4ea0ee663f888294b830e10f94181fd80c1a3c
darktoni94 <tonidominguezreig@gmail.com>
2020-07-22 19:22:32 +0200

    Creates player and ball prefabs and scripts

---
+++ Chicken Project/Assets/Scripts/ShootingBall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingBall : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+
+    private void Start()
+    {
+        StartCoroutine(AutoDestroy());
+    }
+
+    protected IEnumerator AutoDestroy()
+    {
+        yield return new WaitForSeconds(3);
+        Destroy(gameObject);
+    }
+}
